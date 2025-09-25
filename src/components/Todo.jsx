fcd04be2f641b4cda57e5432ce8e40e02cee5e5e
YOYO <abc142536798@gmail.com>
2025-09-26 05:30:08 +0800

    加上todo編輯與完成的功能

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,29 @@
 import { MdDelete, MdEdit } from "react-icons/md";
+import EditForm from "./EditForm";
 
-function Todo({ todo, deleteTodo }) {
-  return (
-    <div className="todo">
-      <p>{todo.content}</p>
+function Todo({ todo, deleteTodo, toggleCompleted, toggleIsEditing, editTodo }) {
+  return todo.isEditing ? (
+    <EditForm todo={todo} editTodo={editTodo}/>
+  ) : (
+    <div className={`todo ${todo.isCompleted ? "completed" : ""}`}>
+      <p
+        onClick={() => {
+          toggleCompleted(todo.id);
+        }}
+      >
+        {todo.content}
+      </p>
       <div>
-        <MdEdit style={{ cursor: "pointer" }} />
-        <MdDelete style={{ cursor: "pointer", marginLeft: "5px" }} onClick={()=>{deleteTodo(todo.id)}} />
+        <MdEdit onClick={()=>{
+            toggleIsEditing(todo.id)
+        }}
+        style={{ cursor: "pointer" }} />
+        <MdDelete
+          style={{ cursor: "pointer", marginLeft: "5px" }}
+          onClick={() => {
+            deleteTodo(todo.id);
+          }}
+        />
       </div>
     </div>
   );
