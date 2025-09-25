13f67960fb8e02d326b115ef826c6d84e7decb98
YOYO <abc142536798@gmail.com>
2025-09-26 01:45:19 +0800

    輸入框讓他可以輸入 且使用開發者工具 最後用雙向綁定

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function CreateForm() {
+  const [content, setContent] = useState("");
+
   return (
     <form className="create-form">
-      <input type="text" placeholder="輸入待辦事項" />
+      <input
+        type="text"
+        placeholder="輸入待辦事項"
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+      />
       <button type="submit">加入</button>
     </form>
   );
